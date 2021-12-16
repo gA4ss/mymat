7af3996efd6e9095ae122c701740bc892dad081a
yanwenbin <yanwenbin@nagain.com>
2021-12-16 12:04:38 +0800

    临时测试

--- src/test/test.cc
+++ src/test/test.cc
@@ -2,6 +2,14 @@
 // #include <mynum/integer.h>
 // #include <mynum/float.h>
 
+class TestType {
+public:
+  TestType() { std::cout << "12" << std::endl; }
+  ~TestType() {}
+};
+
+typedef unsigned long ll_t;
+typedef unsigned int ii_t;
 int main(int argc, char* argv[]) {
   // mynum::float_t a = 0.0;
   double d = 1.0;
@@ -9,11 +17,22 @@ int main(int argc, char* argv[]) {
   long e = 123;
   // mynum::Integer i = "0";
   // mynum::Float f = "0";
-
+  int i = 12;
+  ll_t x = 0;
+  ii_t y = 0;
+  unsigned int z = 0;
+  char c = 1;
+  TestType tt;
   // std::cout << typeid(a).name() << std::endl;
   std::cout << typeid(d).name() << std::endl;
   // std::cout << typeid(c).name() << std::endl;
   std::cout << typeid(e).name() << std::endl;
+  std::cout << typeid(i).name() << std::endl;
+  std::cout << typeid(x).name() << std::endl;
+  std::cout << typeid(y).name() << std::endl;
+  std::cout << typeid(z).name() << std::endl;
+  std::cout << typeid(c).name() << std::endl;
+  std::cout << typeid(tt).name() << std::endl;
   // std::cout << typeid(i).name() << std::endl;
   // std::cout << typeid(f).name() << std::endl;
 
