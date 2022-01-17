54f6bf89f6a4e58742d05cbf9808bdb510d8867b
yanwenbin <yanwenbin@nagain.com>
2022-01-17 20:26:58 +0800

    增加了fraction_str函数

---
+++ src/math/fraction/fraction_str.cc
@@ -0,0 +1,26 @@
+#include <mymat/math/fraction.h>
+
+namespace mymat {
+namespace math {
+
+fraction_t fraction_str(const std::vector<std::vector<fraction_t> >& fmat) {
+  intfr_t d = x.second * y.second;
+  intfr_t n = (x.first * y.second) + (y.first * x.second);
+  intfr_t g = std::__gcd(n, d);
+  n /= g;
+  d /= g;
+  return {n, d};
+}
+
+mp_fraction_t mp_fraction_str(const std::vector<std::vector<mp_fraction_t> >& fmat) {
+  mynum::Integer d = mynum::Integer(x.second) * mynum::Integer(y.second);
+  mynum::Integer n = (mynum::Integer(x.first) * mynum::Integer(y.second)) + 
+                     (mynum::Integer(y.first) * mynum::Integer(x.second));
+  mynum::Integer g = mynum::gcd(n, d);
+  n /= g;
+  d /= g;
+  return {n.str(), d.str()};
+}
+
+} // namespace math
+} // namespace mymat
